adb0de73ce7e5f1fe9f423bbdb5cdc2212e8e42f
Marshall Friskics-Warren <wmfwarren@gmail.com>
2016-07-29 09:50:43 -0500

    began making function for clacs

--- app/controllers/calcCTRL.js
+++ app/controllers/calcCTRL.js
@@ -1,6 +1,6 @@
 "use strict";
 
-app.controller("calcCTRL", function ($scope, $route, dataFactory) {
+app.controller("calcCTRL", function ($scope, $route, dataFactory, objectService) {
 	$scope.shootingBool = true;
 	$scope.infantryBool = true;
 	//To hit
@@ -25,4 +25,66 @@ app.controller("calcCTRL", function ($scope, $route, dataFactory) {
 	$scope.successesShooting = 1;
 	$scope.successesVsInfantry = 1;
 	$scope.firstSaveSuccesses = 1;
+
+//this function determines that attack type
+	function attackSequenceType() {
+		if($scope.shootingBool){
+			shootingAttack($scope.ballisticSkill, $scope.numOfDiceShooting);
+		} else {
+			closeCombatAttack();
+		}
+	}
+
+	function shootingAttack(BSkill, dice) {
+		let target = null;
+		let rerollGrtFive = false; //rerolls if the BS > 5
+		let rerollTarget = null;
+
+		//evaluate the ballistic Skill and auto-hit characteristic
+		if(autoHit){
+			$scope.successesShooting = dice;
+			return {
+				shooting: true,
+				hitTarget: null,
+				autoHit: true,
+				rerollTarget: null,
+				rerollShooting: false
+			};
+		} else if (BSkill > 5) {
+			target = 7 - BSkill;
+			rerollTarget = 6 - (BSkill - 6);
+			rerollGrtFive = true;
+			return {
+				shooting: true,
+				hitTarget: target,
+				autoHit: false,
+				rerollTarget: rerollTarget,
+				rerollShooting: true
+			};
+		} else {
+			target = 7 - BSkill;
+			return {
+				shooting: true,
+				hitTarget: target,
+				autoHit: false,
+				rerollTarget: null,
+				rerollShooting: false
+			};
+		} 
+	}
+
+	function closeCombatAttack() {
+		const weaponSkillArray = [
+		[4,4,5,5,5,5,5,5,5,5],
+		[3,4,4,4,5,5,5,5,5,5],
+		[3,3,4,4,4,4,5,5,5,5],
+		[3,3,3,4,4,4,4,4,5,5],
+		[3,3,3,3,4,4,4,4,4,4],
+		[3,3,3,3,3,4,4,4,4,4],
+		[3,3,3,3,3,3,4,4,4,4],
+		[3,3,3,3,3,3,3,4,4,4],
+		[3,3,3,3,3,3,3,3,4,4],
+		[3,3,3,3,3,3,3,3,3,4]];
+	}
+
 });
